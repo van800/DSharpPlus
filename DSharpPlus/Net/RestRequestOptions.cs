a27b63ffbfa4ec3c14a2c2066132b41af39d4698
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-12-19 15:19:00 -0500

    feat(core.net): Add RequestOptions class.

---
+++ DSharpPlus/Net/RestRequestOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DSharpPlus.Net
+{
+    public class RestRequestOptions
+    {
+        public CancellationToken CancellationToken { internal get; set; } = CancellationToken.None;
+
+        public RetryMode RetryMode { internal get; set; } = RetryMode.RetryIfRatelimited;
+    }
+
+    public enum RetryMode
+    {
+        RetryIfUnsuccessful,
+
+        RetryIfRatelimited,
+        
+        ThrowIfUnsuccessful, 
+    }
+}
